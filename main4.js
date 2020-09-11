0a798944ecdeadd9fa8ebe6335fde0ff3fa2bbf1
Md.Asif Ur Rahman <45636891+asifurrahman01714@users.noreply.github.com>
2020-09-11 20:33:19 +0600

    Add files via upload

---
+++ main4.js
@@ -0,0 +1,15 @@
+var num=prompt("enter the number: ");
+ if(num>=80)
+ document.write("The Grade is A+");
+ else if(num>=70 && num<80)
+ document.write("the grade is A");
+ else if(num>=60 && num<70)
+ document.write("the grade is A-");
+ else if(num>=50 && num<60)
+ document.write("the grade is B");
+ else if(num>=40 && num<50)
+ document.write("the grade is C");
+ else if(num>=33 && num<40)
+ document.write("the grade is D");
+ else
+ document.write("Fail");
